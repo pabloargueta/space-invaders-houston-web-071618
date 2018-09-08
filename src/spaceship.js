dd548e32551a26e46a0ee46e9dcaf72448cd18fb
pabloargueta <pabloargueta27@gmail.com>
2018-09-08 00:30:38 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,20 @@
-class Spaceship {}
+class Spaceship {
+    constructor(name, crew) {
+        this.name = name
+        this.crew = crew
+        this.phasers = 5
+        this.shields = 4
+        this.cloaked = false
+        this.warpDrive = "disengaged"
+        this.phasersCharge = "uncharged"
+        this.docked = crew.length ? false : true
+        this.setCrew()
+    }
+
+    setCrew() {
+
+        this.crew.forEach(member => {
+            member.currentShip = this
+        });
+    }
+}
